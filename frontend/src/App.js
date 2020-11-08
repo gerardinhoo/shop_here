509357723369709a8ac9c3fe0b11daf0a8a7c661
Gerard Eklu <gerard_eklu@homedepot.com>
2020-11-07 19:34:58 -0500

    Implemented the react bootstrap setup - Header and Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+
 
 
 const App = () => {
   return (
-    <div className="App">
-     Shop Here
-    </div>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1 className="py-3">Bienvenue A La Boutique</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
