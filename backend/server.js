44898998eed4a8eaa66f5980c78b24ab87732401
Gerard Eklu <gerard_eklu@homedepot.com>
2020-11-28 12:29:24 -0500

    Mongo db atlas & compass setup - connection to database - added colors to console

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require("dotenv");
 const products = require('./data/products');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -18,8 +22,5 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 })
 
-
 const PORT = process.env.PORT || 5000;
-
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT} `));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.underline.bold));
