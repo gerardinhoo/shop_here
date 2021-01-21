c73acefb2bbb1d603f33134cee7442895e6207b3
Gerard Eklu <gerard_eklu@homedepot.com>
2021-01-20 20:31:40 -0500

    Started adding the shopping cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import HomeScreen from '../src/screens/HomeScreen';
 import ProductScreen from '../src/screens/ProductScreen';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import CartScreen from '../src/screens/CartScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Container>
           <Route path='/' component={HomeScreen} exact></Route>
           <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route path='/cart/:id?' component={CartScreen}></Route>
         </Container>
       </main>
       <Footer />
